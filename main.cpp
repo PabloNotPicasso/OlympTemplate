331185b159c6f1dcad92adc090412e2b7f0bc1c3
PabloNotPicasso <bugaev99@gmail.com>
2021-09-13 00:25:12 +0300

    Added codeformatter

--- main.cpp
+++ main.cpp
@@ -1,37 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 namespace {
-template< template<typename, typename> typename Container, typename T, typename D>
-void _Db(const Container<T,D>& c, const string name){
+template<template<typename, typename> typename Container, typename T, typename D>
+void _Db(const Container<T, D>& c, const string name)
+{
     cout << name << " : ";
-    for( const auto& i : c) cout << i << " ";
+    for (const auto& i : c)
+        cout << i << " ";
     cout << '\n';
 }
 template<typename T>
-void _Db(const T& c, const string name){
+void _Db(const T& c, const string name)
+{
     cout << name << " : " << c << '\n';
 }
 #ifdef DEBUG
-    #define Db(a) _Db(a, #a);
+#define Db(a) _Db(a, #a);
 #else
-    #define Db(a) ;
+#define Db(a) ;
 #endif
-#define FAST_INPUT ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define FAST_INPUT                                                                                 \
+    ios_base::sync_with_stdio(false);                                                              \
+    cin.tie(NULL);
 #define cont(v) v.begin(), v.end()
 #define rcont(v) v.rbegin(), v.rend()
-template<typename T>
-void setMax( T& toSet, const T& toCmp){
-    if( toSet < toCmp )
-        toSet = toCmp;
-}
-template<typename T>
-void setMin( T& toSet, const T& toCmp){
-    if( toSet > toCmp )
-        toSet = toCmp;
-}
 typedef long long ll;
-typedef pair<int,int> pii;
-}
+typedef pair<int, int> pii;
+} // namespace
 ///// CODE :
 
 int main()
